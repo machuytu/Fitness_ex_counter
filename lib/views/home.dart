d1fbb433c9e71cecfee99180037f5dbec4e71a45
TQP0403 <fantasy.tqp@gmail.com>
2021-04-10 13:19:39 +0700

    bmr revision

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -158,7 +158,7 @@ class _HomeState extends State<Home> {
                                   cameras: cameras,
                                   maxs: getListMaxCount(
                                     _user.weight,
-                                    325,
+                                    _user.bmrInt,
                                     [...exercises, ...exercises],
                                   ),
                                 ),
