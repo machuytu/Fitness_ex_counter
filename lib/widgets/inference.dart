7336f7b816a2926bad83f1da60eaa6a66466b43c
TQP0403 <fantasy.tqp@gmail.com>
2021-04-20 11:29:40 +0700

    clean code InferencePage

--- lib/widgets/inference.dart
+++ lib/widgets/inference.dart
@@ -40,9 +40,33 @@ class _InferencePageState extends State<InferencePage> {
   int _previewH;
   int _previewW;
   int _count;
+  Workout _workout;
 
   Exercise get _exercise => _workout?.exercise ?? widget.exercise;
-  Workout _workout;
+
+  Color get _getCounterColor => (_isCorrectPosture) ? Colors.green : Colors.red;
+
+  Color get _getImageColor =>
+      (_isCorrectPosture) ? Colors.transparent : Colors.black.withOpacity(0.5);
+
+  String get _countStr =>
+      (_workout != null) ? '${_workout.count} / ${_workout.max}' : '$_count';
+
+  String get _assetName {
+    String name;
+    switch (_exercise.id) {
+      case 3:
+        name = 'lie_pose';
+        break;
+      case 1:
+        name = 'push_up_pose';
+        break;
+      default:
+        name = 'stand_pose';
+        break;
+    }
+    return 'assets/poses/$name.png';
+  }
 
   @override
   void initState() {
@@ -90,10 +114,9 @@ class _InferencePageState extends State<InferencePage> {
             setRecognitions: _setRecognitions,
           ),
           Center(
-            child: Image(
-              image: AssetImage(_assetName()),
-              color: Colors.black.withOpacity(_isCorrectPosture ? 0.0 : 0.5),
-              colorBlendMode: BlendMode.dstIn,
+            child: Image.asset(
+              _assetName,
+              color: _getImageColor,
             ),
           ),
           Stack(children: _renderHelperBlobs()),
@@ -105,12 +128,10 @@ class _InferencePageState extends State<InferencePage> {
         height: 85,
         width: 85,
         child: FloatingActionButton(
-          backgroundColor: getCounterColor(),
+          backgroundColor: _getCounterColor,
           onPressed: () {},
           child: Text(
-            _workout != null
-                ? '${_workout.count} / ${_workout.max}'
-                : '$_count',
+            _countStr,
             style: TextStyle(
               fontWeight: FontWeight.bold,
               color: Colors.white,
@@ -122,189 +143,15 @@ class _InferencePageState extends State<InferencePage> {
     );
   }
 
-  String _assetName() {
-    String name = (_exercise.id == 3)
-        ? 'lie_pose'
-        : ((_exercise.id == 1) ? 'push_up_pose' : 'stand_pose');
-
-    return 'assets/poses/$name.png';
-  }
-
-  void _increaseCount() {
-    if (_workout != null) {
-      _workout.increaseCount(
-        fnCount: (value) {
-          _flutterTts.speak(value.toString());
-          setState(() {});
-        },
-        fnMax: () {
-          _flutterTts.speak("Change exercise!");
-          _setRangeBasedOnModel();
-          setState(() {});
-        },
-        fnDone: () {
-          _flutterTts.speak("Your workout done!");
-          Get.back();
-        },
-      );
-    } else {
-      setState(() {
-        _count++;
-      });
-      _flutterTts.speak(_count.toString());
-    }
-  }
-
-  void _setMidCount(bool f) {
-    //when _midCount is activated
-    if (f && !_midCount) {
-      _flutterTts.speak("Perfect!");
-    }
-    setState(() {
-      _midCount = f;
-    });
-  }
-
-  Color getCounterColor() {
-    if (_isCorrectPosture) {
-      return Colors.green;
-    } else {
-      return Colors.red;
-    }
-  }
-
   Positioned _createPositionedBlobs(double h, double w, double x, double y) {
     return Positioned(
       height: h,
       width: w,
       left: x,
       top: y,
-      child: Container(
-        color: getCounterColor(),
-      ),
+      child: Container(color: _getCounterColor),
     );
   }
-
-  List<Widget> _renderHelperBlobs() {
-    List<Widget> listToReturn = <Widget>[];
-    if (_exercise.id != 3) {
-      listToReturn.add(_createPositionedBlobs(5, 40, 0, _upperRange));
-      listToReturn.add(_createPositionedBlobs(5, 40, 0, _lowerRange));
-    } else {
-      listToReturn.add(_createPositionedBlobs(40, 5, _upperRange, 0));
-      listToReturn.add(_createPositionedBlobs(40, 5, _lowerRange, 0));
-    }
-    return listToReturn;
-  }
-
-  void _setRangeBasedOnModel() {
-    double h = _screenH * 0.8;
-    double w = _screenW * 0.8;
-    if (_exercise.id == 0) {
-      _upperRange = h * 0.30; // pixel get on screen
-      _lowerRange = h * 0.60;
-    } else if (_exercise.id == 1) {
-      _upperRange = h * 0.45;
-      _lowerRange = h * 0.65;
-    } else if (_exercise.id == 2) {
-      _upperRange = h * 0.30;
-      _lowerRange = h * 0.85;
-    } else if (_exercise.id == 3) {
-      _upperRange = w * 0.50;
-      _lowerRange = w * 1.00;
-    }
-  }
-
-  //region Core
-  bool _postureAccordingToExercise(Map<String, List<double>> poses) {
-    switch (_exercise.id) {
-      case 0:
-        return poses['leftShoulder'][1] < _upperRange &&
-            poses['rightShoulder'][1] < _upperRange &&
-            poses['leftHip'][1] < _lowerRange &&
-            poses['rightHip'][1] < _lowerRange;
-        break;
-      case 1:
-        return poses['leftShoulder'][1] > _upperRange &&
-            poses['rightShoulder'][1] > _upperRange &&
-            poses['leftShoulder'][1] < _lowerRange &&
-            poses['rightShoulder'][1] < _lowerRange;
-        break;
-      case 2:
-        return poses['leftShoulder'][1] < _upperRange &&
-            poses['rightShoulder'][1] < _upperRange;
-        break;
-      case 3:
-        return poses['rightShoulder'][0] > _upperRange &&
-            poses['rightShoulder'][0] < _lowerRange;
-        break;
-      default:
-        return null;
-        break;
-    }
-  }
-
-  bool _midPostureExercise(Map<String, List<double>> poses) {
-    switch (_exercise.id) {
-      case 0:
-        return poses['leftShoulder'][1] > _upperRange &&
-            poses['rightShoulder'][1] > _upperRange;
-        break;
-      case 1:
-        return poses['leftShoulder'][1] > _lowerRange &&
-            poses['rightShoulder'][1] > _lowerRange;
-        break;
-      case 2:
-        return poses['leftShoulder'][1] > _upperRange &&
-            poses['rightShoulder'][1] > _upperRange &&
-            (poses['leftKnee'][1] > _lowerRange ||
-                poses['rightKnee'][1] > _lowerRange) &&
-            (poses['leftKnee'][1] < _lowerRange ||
-                poses['rightKnee'][1] < _lowerRange);
-        break;
-      case 3:
-        return poses['rightShoulder'][0] < _upperRange;
-        break;
-      default:
-        return null;
-        break;
-    }
-  }
-
-  void _checkCorrectPosture(Map<String, List<double>> poses) {
-    if (_postureAccordingToExercise(poses)) {
-      if (!_isCorrectPosture) {
-        setState(() {
-          _isCorrectPosture = true;
-        });
-      }
-    } else {
-      if (_isCorrectPosture) {
-        setState(() {
-          _isCorrectPosture = false;
-        });
-      }
-    }
-  }
-
-  void _countingLogic(Map<String, List<double>> poses) {
-    if (poses != null) {
-      //check posture before beginning count
-      if (_isCorrectPosture && _midPostureExercise(poses)) {
-        _setMidCount(true);
-      }
-      if (_midCount && _postureAccordingToExercise(poses)) {
-        _increaseCount();
-        _setMidCount(false);
-      }
-      //check the posture when not in _midCount
-      if (!_midCount) {
-        _checkCorrectPosture(poses);
-      }
-    }
-  }
-
-  //endregion
 
   List<Widget> _renderKeypoints() {
     var lists = <Widget>[];
@@ -342,8 +189,8 @@ class _InferencePageState extends State<InferencePage> {
             left: x - 250,
             top: y - 50,
             child: Container(
-              width: 8,
-              height: 8,
+              width: 5,
+              height: 5,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color: Color.fromRGBO(37, 213, 253, 1.0),
@@ -359,6 +206,27 @@ class _InferencePageState extends State<InferencePage> {
     return lists;
   }
 
+  List<Widget> _renderHelperBlobs() {
+    List<Widget> listToReturn = <Widget>[];
+    if (_exercise.id != 3) {
+      listToReturn.add(_createPositionedBlobs(5, 40, 0, _upperRange));
+      listToReturn.add(_createPositionedBlobs(5, 40, 0, _lowerRange));
+    } else {
+      listToReturn.add(_createPositionedBlobs(40, 5, _upperRange, 0));
+      listToReturn.add(_createPositionedBlobs(40, 5, _lowerRange, 0));
+    }
+    return listToReturn;
+  }
+
+  void _loadModel() async {
+    await Tflite.loadModel(
+      model: 'assets/models/posenet_mv1_075_float_from_checkpoints.tflite',
+      numThreads: 5, // defaults to 1
+      isAsset: true,
+      useGpuDelegate: true,
+    );
+  }
+
   void _setRecognitions(recognitions, imageHeight, imageWidth) {
     if (!mounted) {
       return;
@@ -372,12 +240,161 @@ class _InferencePageState extends State<InferencePage> {
     });
   }
 
-  void _loadModel() async {
-    await Tflite.loadModel(
-      model: 'assets/models/posenet_mv1_075_float_from_checkpoints.tflite',
-      numThreads: 5, // defaults to 1
-      isAsset: true,
-      useGpuDelegate: true,
-    );
+  void _increaseCount() {
+    if (_workout != null) {
+      _workout.increaseCount(
+        fnCount: (value) {
+          _flutterTts.speak(value.toString());
+          setState(() {});
+        },
+        fnMax: () {
+          _flutterTts.speak("Change exercise!");
+          _setRangeBasedOnModel();
+          setState(() {});
+        },
+        fnDone: () {
+          _flutterTts.speak("Your workout done!");
+          Get.back();
+        },
+      );
+    } else {
+      setState(() {
+        _count++;
+      });
+      _flutterTts.speak(_count.toString());
+    }
   }
+
+  void _setMidCount(bool f) {
+    //when _midCount is activated
+    if (f && !_midCount) {
+      _flutterTts.speak("Perfect!");
+    }
+    setState(() {
+      _midCount = f;
+    });
+  }
+
+  void _setRangeBasedOnModel() {
+    final h = _screenH * 0.8 - 5;
+    final w = _screenW - 5;
+
+    switch (_exercise.id) {
+      case 0:
+        _upperRange = h * 0.35;
+        _lowerRange = h * 0.75;
+        break;
+      case 1:
+        _upperRange = h * 0.45;
+        _lowerRange = h * 0.65;
+        break;
+      case 2:
+        _upperRange = h * 0.35;
+        _lowerRange = h * 0.90;
+        break;
+      case 3:
+        _upperRange = w * 0.30;
+        _lowerRange = w * 0.70;
+        break;
+      default:
+        break;
+    }
+  }
+
+  //region Core
+  bool _postureAccordingToExercise(Map<String, List<double>> poses) {
+    switch (_exercise.id) {
+      case 0:
+        return poses['leftShoulder'][1] < _upperRange &&
+            poses['rightShoulder'][1] < _upperRange &&
+            poses['leftHip'][1] < _lowerRange &&
+            poses['rightHip'][1] < _lowerRange;
+        break;
+      case 1:
+        return poses['leftShoulder'][1] > _upperRange &&
+            poses['rightShoulder'][1] > _upperRange &&
+            poses['leftShoulder'][1] < _lowerRange &&
+            poses['rightShoulder'][1] < _lowerRange;
+        break;
+      case 2:
+        return poses['leftShoulder'][1] < _upperRange &&
+            poses['rightShoulder'][1] < _upperRange;
+        break;
+      case 3:
+        return poses['rightShoulder'][0] > _upperRange &&
+            poses['rightShoulder'][0] < _lowerRange;
+        break;
+      default:
+        return false;
+        break;
+    }
+  }
+
+  bool _midPostureExercise(Map<String, List<double>> poses) {
+    switch (_exercise.id) {
+      case 0:
+        return poses['leftShoulder'][1] > _upperRange &&
+            poses['rightShoulder'][1] > _upperRange;
+        break;
+      case 1:
+        return poses['leftShoulder'][1] > _lowerRange &&
+            poses['rightShoulder'][1] > _lowerRange;
+        break;
+      case 2:
+        return poses['leftShoulder'][1] > _upperRange &&
+            poses['rightShoulder'][1] > _upperRange &&
+            (poses['leftKnee'][1] > _lowerRange ||
+                poses['rightKnee'][1] > _lowerRange) &&
+            (poses['leftKnee'][1] < _lowerRange ||
+                poses['rightKnee'][1] < _lowerRange);
+        break;
+      case 3:
+        return poses['rightShoulder'][0] < _upperRange;
+        break;
+      default:
+        return false;
+        break;
+    }
+  }
+
+  void _checkCorrectPosture(Map<String, List<double>> poses) {
+    if (_isCorrectPosture != _postureAccordingToExercise(poses)) {
+      setState(() {
+        _isCorrectPosture = _postureAccordingToExercise(poses);
+      });
+    }
+    // if (_postureAccordingToExercise(poses)) {
+    //   if (!_isCorrectPosture) {
+    //     setState(() {
+    //       _isCorrectPosture = true;
+    //     });
+    //   }
+    // } else {
+    //   if (_isCorrectPosture) {
+    //     setState(() {
+    //       _isCorrectPosture = false;
+    //     });
+    //   }
+    // }
+  }
+
+  void _countingLogic(Map<String, List<double>> poses) {
+    if (poses != null) {
+      //check posture before beginning count
+      if (_isCorrectPosture && _midPostureExercise(poses)) {
+        _setMidCount(true);
+      }
+      if (_midCount && _postureAccordingToExercise(poses)) {
+        _increaseCount();
+        _setMidCount(false);
+      }
+      //check the posture when not in _midCount
+      if (!_midCount) {
+        _checkCorrectPosture(poses);
+      }
+    }
+  }
+
+  //endregion
+
 }
